746c1460c86ee60344ba08f89ab75f8bcdf91eea
scherepi <scherejoaquin@gmail.com>
2025-12-05 14:49:07 -0600

    Restructured to create a proper project

---
+++ day1/src/main.rs
@@ -0,0 +1,8 @@
+use std::fs::File;
+
+fn main() {
+    println!("Advent of Code 2025: Day 1!");
+    println!("Retrieving problem data");
+
+    let mut currentValue: i16 = 50; // dial starts at 50
+}
